6ee3d7c1bc3459c574562449cb704847e5dd16f8
root <mendoza1@us.ibm.com>
2020-10-30 14:23:42 -0500

    changes to make code work with VCS instance

--- versions.tf
+++ versions.tf
@@ -1,13 +1,4 @@
 terraform {
   required_version = ">= 0.13"
-  required_providers {
-    vcd = {
-      source = "vmware/vcd"
-      version = "3.0.0"
-    }
-  }
 }
 
-provider "vcd" {
-  # Configuration options
-}
