0c42684bb80289f783831001cdf2e279cdbfb240
root <mendoza1@us.ibm.com>
2020-10-30 15:53:09 -0500

    changes to make code work with VCS instance

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "vcd" {
   org      = var.vcd_org
   url      = var.vcd_url
   vdc      = var.vdc_name
+  version  = "~> 3.0.0"
+  source = "vmware/vcd"
 }
 
 # Used to obtain information from the already deployed Edge Gateway
